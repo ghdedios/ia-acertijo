a3b25506aea86bc3fa9da0e5a4894e18b1ef4de1
Gonzalo de Dios <ghdedios@hotmail.com>
2017-10-16 16:50:46 -0300

    Se crea clase acertijo donde correrá el main

---
+++ src/main/java/ar/edu/utn/frba/ia/acertijo/Acertijo.java
@@ -0,0 +1,19 @@
+package main.java.ar.edu.utn.frba.ia.acertijo;
+
+import java.util.logging.Logger;
+
+import main.java.ar.edu.utn.frba.ia.ag.AlgoritmoGenetico;
+import main.java.ar.edu.utn.frba.ia.ag.ConfiguracionDefault;
+import main.java.ar.edu.utn.frba.ia.ag.Individuo;
+import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.BinomialMascaraComplemento;
+import main.java.ar.edu.utn.frba.ia.ag.ejemplos.maximo.X;
+import main.java.ar.edu.utn.frba.ia.ag.mutacion.MutacionSimple;
+import main.java.ar.edu.utn.frba.ia.ag.seleccion.ControlSobreNumeroEsperado;
+
+
+public class Acertijo {
+
+public static void main(String[] args) {
+		
+	}
+}
