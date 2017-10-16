4a7a7e3c2de5eaa1fcd5a439c960889531ad0a03
Gonzalo de Dios <ghdedios@hotmail.com>
2017-10-16 17:07:04 -0300

    Se agrega configuracion para primer corrida. Metodo de Seleccion = Torneo, Cruzamiento = Simple, Mutacion = Simple con probabilidad 0.01 (teniendo en cuenta que la mutacion deberia ser sacada dado que ya manejamos consistencia en la creacion random y la mutacion puede alterar dicha consistencia y arrojar alternativas invalidas)

--- src/main/java/ar/edu/utn/frba/ia/acertijo/Acertijo.java
+++ src/main/java/ar/edu/utn/frba/ia/acertijo/Acertijo.java
@@ -5,15 +5,28 @@ import java.util.logging.Logger;
 import main.java.ar.edu.utn.frba.ia.ag.AlgoritmoGenetico;
 import main.java.ar.edu.utn.frba.ia.ag.ConfiguracionDefault;
 import main.java.ar.edu.utn.frba.ia.ag.Individuo;
-import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.BinomialMascaraComplemento;
-import main.java.ar.edu.utn.frba.ia.ag.ejemplos.maximo.X;
+import main.java.ar.edu.utn.frba.ia.ag.cruzamiento.Simple;
 import main.java.ar.edu.utn.frba.ia.ag.mutacion.MutacionSimple;
-import main.java.ar.edu.utn.frba.ia.ag.seleccion.ControlSobreNumeroEsperado;
+import main.java.ar.edu.utn.frba.ia.ag.seleccion.Torneo;
 
 
 public class Acertijo {
 
 public static void main(String[] args) {
-		
+
+	ConfiguracionDefault c = new ConfiguracionDefault();
+	
+	c.setMetodoDeSeleccion(new Torneo());
+	c.setCruzamiento(new Simple());
+	c.setMutacion(new MutacionSimple(0.01)); //La mutacion si o si da lugar a resultados invalidos. Analizar sacar mutación
+	
+	AlgoritmoGenetico acertijo = new AlgoritmoGenetico(c, Solucion.class);
+	
+	Individuo solucionFinal = acertijo.ejecutar();
+	
+	Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe("Individuo final: " + solucionFinal.toString());
+	
+	System.out.println("------------------------------------------------------------------------");
+	
 	}
 }
